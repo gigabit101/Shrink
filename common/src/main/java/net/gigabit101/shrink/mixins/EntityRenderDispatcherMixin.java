e263bc3a69f80070bb602e4e21db2fda04b2e0cc
Gigabit101 <thegigabit101@gmail.com>
2024-03-24 04:49:13 +0000

    Remove shadow render for now

--- common/src/main/java/net/gigabit101/shrink/mixins/EntityRenderDispatcherMixin.java
+++ common/src/main/java/net/gigabit101/shrink/mixins/EntityRenderDispatcherMixin.java
@@ -18,29 +18,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin
 {
-    @Unique
-    private static float shrink$value = 0;
-    @Inject(method = "renderShadow", at = @At("HEAD"))
-    private static void renderShadow(PoseStack poseStack, MultiBufferSource multiBufferSource, Entity entity, float f, float g, LevelReader levelReader, float h, CallbackInfo ci)
-    {
-        if(entity instanceof ItemEntity) return;
-
-        if(entity instanceof LivingEntity livingEntity)
-        {
-            if(ShrinkAPI.isEntityShrunk(livingEntity))
-            {
-                shrink$value = (float) (h * livingEntity.getAttribute(ShrinkAPI.SCALE_ATTRIBUTE).getValue());
-            }
-            else
-            {
-                shrink$value = h;
-            }
-        }
-    }
-
-    @ModifyVariable(method = "renderShadow", at = @At("HEAD"), ordinal = 2)
-    private static float injected(float h)
-    {
-        return shrink$value;
-    }
+//    @Unique
+//    private static float shrink$value = 0;
+//    @Inject(method = "renderShadow", at = @At("HEAD"))
+//    private static void renderShadow(PoseStack poseStack, MultiBufferSource multiBufferSource, Entity entity, float f, float g, LevelReader levelReader, float h, CallbackInfo ci)
+//    {
+//        if(entity instanceof ItemEntity) return;
+//
+//        if(entity instanceof LivingEntity livingEntity)
+//        {
+//            if(ShrinkAPI.isEntityShrunk(livingEntity))
+//            {
+//                shrink$value = (float) (h * livingEntity.getAttribute(ShrinkAPI.SCALE_ATTRIBUTE).getValue());
+//            }
+//            else
+//            {
+//                shrink$value = h;
+//            }
+//        }
+//    }
+//
+//    @ModifyVariable(method = "renderShadow", at = @At("HEAD"), ordinal = 2)
+//    private static float injected(float h)
+//    {
+//        return shrink$value;
+//    }
 }
